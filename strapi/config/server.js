826dfb0c1544dc734252f74be19d73b2e9686999
Raoul Kramer <git@raoulkramer.de>
2020-10-23 12:04:17 +0200

    strapi quickstart installation; add page

---
+++ strapi/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'bc6ff37fdaf41b9131046620d482a5af'),
+    },
+  },
+});
