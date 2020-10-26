25bbfe460efb46d20b85731623821ecb4ef8f305
Raoul Kramer <git@raoulkramer.de>
2020-10-26 10:51:14 +0100

    change slug creation

--- strapi/api/page/models/page.js
+++ strapi/api/page/models/page.js
@@ -9,14 +9,22 @@ async function createPageSlug(model) {
   let slugPrefix = '/';
   let parentPage;
   let pageSlug = '';
+  // check if page is the index page
+  if (model.indexPage) {
+    return slugPrefix;
+  }
+  // keep the user entered slug, ensure it's url-friendly
+  if (model.fixSlug) {
+    return `${slugPrefix}${model.slug.split('/').map((partial) => slugify(partial)).join('/')}`.replace(/\/[\/]*/g, '/');
+  }
   // check if a parent page is set, so prefix the parent pages slug
   if (model.parentPage) {
     parentPage = await strapi.query('page').findOne({ id: model.parentPage });
-    slugPrefix = parentPage.slug;
+    slugPrefix = `${parentPage.slug}/`;
   }
-  // create page slug
-  if (model.slug === '') {
-    pageSlug = `${slugPrefix}${slugify(model.name)}`;
+  // create slug from page title
+  if (!model.slug || model.slug === '') {
+    pageSlug = `${slugPrefix}${slugify(model.title)}`;
   } else {
     // if a parent page exists, only reuse last part of slug
     if (parentPage) {
@@ -36,12 +44,12 @@ async function createPageSlug(model) {
 module.exports = {
   lifecycles: {
     async beforeCreate(model) {
-      if (model.name) {
+      if (model.title) {
         model.slug = await createPageSlug(model);
       }
     },
     async beforeUpdate(params, model) {
-      if (model.slug || model.name) {
+      if (model.title || model.slug) {
         model.slug = await createPageSlug(model);
       }
     }
