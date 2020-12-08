bc5ee5c1ca8e94f6b46f05a9d5c57a1c603389bf
Raoul Kramer <git@raoulkramer.de>
2020-12-08 10:40:28 +0100

    add code splitting for strapi components

--- sapper/src/components/strapi/Strapi.svelte
+++ sapper/src/components/strapi/Strapi.svelte
@@ -1,18 +1,51 @@
 <script>
-  import SimpleText from './SimpleText.svelte';
-  import SimpleImage from './SimpleImage.svelte';
+  /**
+   * StrapiComponents
+   * by `import SimpleText from './SimpleText.svelte'` all strapi components
+   * will end up in one strapi component.
+   * This class uses "dynamic" imports to force build process to split the components
+   * into their own files.
+   * If you add a new strapi component, you need to extend the switch statement with this component.
+   *
+   * With this setup a _costy_ component will only land in your visitors browser if it is displayed.
+   */
+  class StrapiComponents {
+    constructor() {
+      this._components = {};
+    }
 
-  const componentMap = {
-    'page.simple-text': SimpleText,
-    'page.simple-image': SimpleImage,
-  };
+    async get(component) {
+      if (this._components[component]) {
+        return this._components[component]
+      }
+
+      switch (component) {
+        case 'page.simple-text':
+          this._components[component] = await import('./SimpleText.svelte');
+          break;
+        case 'page.simple-image':
+          this._components[component] = await import('./SimpleImage.svelte');
+          break;
+        default:
+          throw Error(`Component "${component}" unknown`);
+      }
+
+      return this.get(component);
+    }
+  }
+
+  const strapiComponents = new StrapiComponents();
 
   export let contents = [];
 </script>
 
 {#each contents as content}
+{#await strapiComponents.get(content.__component) then c}
 <svelte:component
-  this={componentMap[content.__component]}
+  this={c.default}
   {content}
 />
+{:catch error}
+	<p style="color: red">{error.message}</p>
+{/await}
 {/each}
