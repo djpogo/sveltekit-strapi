72c5b1f930cebb22114f673549a70e3619652056
raoul <git@raoulkramer.de>
2021-03-02 09:56:22 +0100

    better handling of failing api calls

--- sapper/src/strapi.js
+++ sapper/src/strapi.js
@@ -4,11 +4,13 @@ export async function strapiPreload(page, session) {
   const res = await this.fetch(`${apiUrl}/pages?slug=${encodeURIComponent(page.path)}`);
   const data = await res.json();
   if (res.status !== 200) {
-    this.error(res.status, data.message);
+    this.error(res.status, `[${res.status}] ${decodeURIComponent(res.url)} : ${data.message}`);
+    return;
   }
   // empty array from strapi results in a 404 page
   if (!data || data.length === 0) {
     this.error(404, 'Page not found');
+    return;
   }
   return {
     pageData: data.shift(),
@@ -19,7 +21,10 @@ export async function navPreload(page, session) {
   const res = await this.fetch(`${apiUrl}/navigation`);
   const data = await res.json();
   if (res.status !== 200) {
-    this.error(res.status, data.message);
+    console.info(res.status, `[${res.status}] ${decodeURIComponent(res.url)} : ${data.message}`);
+    return {
+      navPages: [],
+    };
   }
   return {
     navPages: data.Navigation.map((page) => ({
