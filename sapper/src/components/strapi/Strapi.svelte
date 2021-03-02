1291a55f70a7904232d1ef9922501376a5559fc5
raoul <git@raoulkramer.de>
2021-03-02 09:58:56 +0100

    remove "Simple" in svelte too. Fix SSR issue in Strapi component

--- sapper/src/components/strapi/Strapi.svelte
+++ sapper/src/components/strapi/Strapi.svelte
@@ -1,51 +1,18 @@
 <script>
-  /**
-   * StrapiComponents
-   * by `import SimpleText from './SimpleText.svelte'` all strapi components
-   * will end up in one strapi component.
-   * This class uses "dynamic" imports to force build process to split the components
-   * into their own files.
-   * If you add a new strapi component, you need to extend the switch statement with this component.
-   *
-   * With this setup a _costy_ component will only land in your visitors browser if it is displayed.
-   */
-  class StrapiComponents {
-    constructor() {
-      this._components = {};
-    }
+  import Text from './Text.svelte';
+  import Image from './Image.svelte';
 
-    async get(component) {
-      if (this._components[component]) {
-        return this._components[component]
-      }
-
-      switch (component) {
-        case 'page.simple-text':
-          this._components[component] = await import('./SimpleText.svelte');
-          break;
-        case 'page.simple-image':
-          this._components[component] = await import('./SimpleImage.svelte');
-          break;
-        default:
-          throw Error(`Component "${component}" unknown`);
-      }
-
-      return this.get(component);
-    }
-  }
-
-  const strapiComponents = new StrapiComponents();
+  const componentMap = {
+    'page.text': Text,
+    'page.image': Image,
+  };
 
   export let contents = [];
 </script>
 
 {#each contents as content}
-{#await strapiComponents.get(content.__component) then c}
 <svelte:component
-  this={c.default}
+  this={componentMap[content.__component]}
   {content}
 />
-{:catch error}
-	<p style="color: red">{error.message}</p>
-{/await}
 {/each}
