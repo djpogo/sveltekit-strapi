0e49c9d3a355ab9aec60caae6e91f251692fd6d1
raoul <git@raoulkramer.de>
2021-03-21 12:02:11 +0100

    add Nav and HtmlHead component

---
+++ sveltejskit/src/lib/HtmlHead.svelte
@@ -0,0 +1,12 @@
+<script>
+  const apiUrl = 'http://localhost:1337'; // TODO put apiEndpoint in .env file
+  export let pageData;
+</script>
+
+<svelte:head>
+  <title>{pageData.title}</title>
+  {#if pageData.description}<meta name="description" content={pageData.description}>{/if}
+  {#if pageData.ogImage}
+  <meta property="og:image" content={`${apiUrl}${pageData.ogImage.url}`}>
+  {/if}
+</svelte:head>
---
+++ sveltejskit/src/lib/Nav.svelte
@@ -0,0 +1,3 @@
+<script>
+  export let navPages;
+</script>
